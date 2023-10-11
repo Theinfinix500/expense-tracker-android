449ff9bc0342cf922b9ec6735e3c76a68362a352
Abdelali Djebara <adjebara@sqli.com>
2023-10-11 15:37:40 +0100

    release: test release apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.theinfinix500.expense-tracker',
   appName: 'expense-tracker',
   webDir: 'www',
   server: {
